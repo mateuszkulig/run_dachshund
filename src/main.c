41d4e79d8258fde6402b3590d004b78d817b6012
Mateusz Kulig <105542191+mateuszkulig@users.noreply.github.com>
2023-03-29 14:15:15 +0200

    temporary switch code in main

--- src/main.c
+++ src/main.c
@@ -2,7 +2,27 @@
 #include "client.h"
 
 int main(int argc, char **argv) {
-    printf("hello world!\n");
-    run(argc, argv);
+    char buffer;
+
+    printf("hello dachshund!\n");
+    printf("please type entity to launch[(c)lient/(s)erver]: ");
+    scanf("%c", &buffer);
+    printf("your choice is %s\n", buffer == 'c' ? "client" : "server");
+    switch (buffer) {
+        case 'c':
+            {
+                const char *sendbuf = "this is a test";
+                platformSocket sock = clientInit();
+                sendData(sock, sendbuf);
+                killSocket(sock);
+                break;
+            }
+        case 's':
+            {   
+                // launch server
+                break;
+            }
+    }
+
     return 0;
 }
