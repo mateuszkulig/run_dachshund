667dbf37373f36ef9e493b27b41a4cc1963c2c1a
Mateusz Kulig <105542191+mateuszkulig@users.noreply.github.com>
2023-05-10 04:32:13 +0200

    render background and 2 blobs

--- inc/engine.h
+++ inc/engine.h
@@ -2,23 +2,23 @@
 
 #define DEFAULT_WINDOW_WIDTH    800
 #define DEFAULT_WINDOW_HEIGHT   600
+#define PLAYER_COUNT            2
 
 // container for SDL stuff
 typedef struct windowData {
     SDL_Window      *window;
     SDL_Renderer    *renderer;
+    SDL_Rect        *background;
 } windowData;
 
 typedef struct playerData {
-    int         x;
-    int         y;
     int         top, bottom;
     int         right, left;
     SDL_Rect    *image;
 } playerData;
 
 typedef struct gameData {
-    playerData  *players[1];
+    playerData  *players[PLAYER_COUNT];
 } gameData;
 
 // create the window and renderer
@@ -27,3 +27,5 @@ windowData *createWindow();
 // main game loop
 void gameLoop();
 
+// init a player
+playerData *addPlayer(int t, int b, int r, int l, SDL_Rect *img);
--- src/engine.c
+++ src/engine.c
@@ -13,7 +13,24 @@ windowData *createWindow() {
     
     result->renderer = SDL_CreateRenderer(result->window, -1, SDL_RENDERER_ACCELERATED);
 
+    result->background = malloc(sizeof(SDL_Rect));
+    result->background->x = 0;
+    result->background->y = 0;
+    result->background->w = DEFAULT_WINDOW_WIDTH;
+    result->background->h = DEFAULT_WINDOW_HEIGHT;
+
     return result;
+}
+
+playerData *addPlayer(int t, int l, int b, int r, SDL_Rect *img) {
+    playerData *player = malloc(sizeof(playerData));
+    player->top = t;
+    player->bottom = b;
+    player->right = r;
+    player->left = l;
+    player->image = img;
+    
+    return player;
 }
 
 void gameLoop() {
@@ -23,7 +40,20 @@ void gameLoop() {
     windowData  *window = createWindow();
     gameData    state;
 
-    
+    SDL_Rect    rectP1, rectP2;
+
+    rectP1.x = 200;
+    rectP1.y = 400;
+    rectP1.w = 100;
+    rectP1.h = 100;
+
+    rectP2.x = 500;
+    rectP2.y = 400;
+    rectP2.w = 100;
+    rectP2.h = 100;
+
+    state.players[0] = addPlayer(200, 400, 100, 200, &rectP1);
+    state.players[1] = addPlayer(500, 400, 100, 600, &rectP2);
     
 
     while (!quit) {
@@ -33,5 +63,16 @@ void gameLoop() {
             }
         }
         
+        // draw background
+        SDL_SetRenderDrawColor(window->renderer, 0, 0, 255, SDL_ALPHA_OPAQUE);
+        SDL_RenderFillRect(window->renderer, window->background);
+
+        // draw players
+        SDL_SetRenderDrawColor(window->renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
+        for (size_t i=0; i<PLAYER_COUNT; ++i) {
+            SDL_RenderFillRect(window->renderer, state.players[i]->image);
+        }
+
+        SDL_RenderPresent(window->renderer);
     }
 }
