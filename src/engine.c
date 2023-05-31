5ecb4b2d22d66805e591f1ec200eedb29f41f44e
Mateusz Kulig <105542191+mateuszkulig@users.noreply.github.com>
2023-05-31 13:06:00 +0200

    change to software rendering; change ip to localhost

--- src/engine.c
+++ src/engine.c
@@ -13,7 +13,7 @@ windowData *createWindow() {
         DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT,
         SDL_WINDOW_SHOWN);
     
-    result->renderer = SDL_CreateRenderer(result->window, -1, SDL_RENDERER_ACCELERATED);
+    result->renderer = SDL_CreateRenderer(result->window, -1, SDL_RENDERER_SOFTWARE);
     result->surface = SDL_GetWindowSurface(result->window);
 
     result->background = malloc(sizeof(SDL_Rect));
